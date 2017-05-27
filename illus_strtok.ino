92b6ed21546fa8ba471cf70c94f05efa98f7b93b
kowus <barnabas@elite-education.org>
2017-05-27 14:22:52 +0000

    for Evans

--- illus_strtok.ino
+++ illus_strtok.ino
@@ -2,12 +2,11 @@
 const char *gpsdata = "$GPGGA,021955.00,0536.59369,N,00015.81379,W,1,08,0.99,45.8,M,,*72";
 char *str, *token, *saveptr;
 String parsedData[14];
-int i;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Serial.println("String Tokens:");
-  for (i = 0, str = gpsdata;; i++, str = NULL) {
+  for (int i = 0, str = gpsdata;; i++, str = NULL) {
     token = strtok_r(str, ",", &saveptr);
     if(token == NULL) break;
     parsedData[i] = token; 
